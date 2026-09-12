c1d1951c18946a4d81dd94851fa3abc732632161
guptaapar21 <guptaapar21@gmail.com>
2026-09-12 20:31:13 +0530

    fix: remove obsolete nightly supervisor workflow check

--- tools/verify_night.py
+++ tools/verify_night.py
@@ -1,5 +1,13 @@
-import os, subprocess, json
-r=os.environ['REPO']
-for wf in ['continuous-collector.yml','process-batch.yml','storage-housekeeping.yml','nightly-supervisor.yml']:
-    x=json.loads(subprocess.check_output(['gh','api',f'repos/{r}/actions/workflows/{wf}'],text=True))
+import json
+import os
+import subprocess
+
+r = os.environ['REPO']
+for wf in ['continuous-collector.yml', 'process-batch.yml', 'storage-housekeeping.yml']:
+    x = json.loads(
+        subprocess.check_output(
+            ['gh', 'api', f'repos/{r}/actions/workflows/{wf}'],
+            text=True,
+        )
+    )
     print(wf, x['state'], x['id'])
